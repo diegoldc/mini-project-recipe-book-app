72485fc95086eed50dece94c80aab250b86d9440
Diego Lázaro <dlazarodecastro@gmail.com>
2024-09-20 18:10:16 +0200

    day 4 done ok

--- recipe-book-app/src/componentes/Sidebar.jsx
+++ recipe-book-app/src/componentes/Sidebar.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom"
+import imgHome from "../assets/home.png"
+import imgAbout from "../assets/about.png"
 
 function Sidebar() {
   return (
     <aside className="sideBar">
-      <Link to="/">Home</Link>
+      <Link to="/"><img src={imgHome} alt="img-home" /></Link>
       
-      <Link to="/about">About Us</Link>
+      <Link to="/about"><img src={imgAbout} alt="img-about" /></Link>
     </aside>
   )
 }
